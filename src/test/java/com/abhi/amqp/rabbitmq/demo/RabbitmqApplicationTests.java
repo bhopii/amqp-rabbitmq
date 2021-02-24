19995478fd49ab9d537fe0c3d027f5a4caab8055
Kar, Abhishek (ak8073) <ak8073@att.com>
2021-02-23 23:15:39 -0500

    Demo Rabbit mq

---
+++ src/test/java/com/abhi/amqp/rabbitmq/demo/RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.amqp.rabbitmq.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
